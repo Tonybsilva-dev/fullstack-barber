c992501becfd29cb4e57cb5930f51bd94477fe7b
Antonio Silva <antoniobsilvaprof@hotmail.com>
2024-02-02 00:01:21 -0300

    feat: add bokking page and canceled appointment is permited

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,12 +1,12 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
-import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
-import BarbershopItem from "./_components/barbershop-item";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import BarbershopItem from "../(home)/_components/barbershop-item";
+import { Search } from "lucide-react";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -34,7 +34,9 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Antonio S!</h2>
+        <h2 className="text-xl font-bold">
+          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+        </h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
@@ -49,9 +51,7 @@ export default async function Home() {
       <div className="mt-6">
         {confirmedBookings.length > 0 && (
           <>
-            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">
-              Agendamentos
-            </h2>
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
             <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
               {confirmedBookings.map((booking) => (
                 <BookingItem key={booking.id} booking={booking} />
@@ -62,9 +62,7 @@ export default async function Home() {
       </div>
 
       <div className="mt-6">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
-          Recomendados
-        </h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
@@ -74,9 +72,7 @@ export default async function Home() {
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
-          Populares
-        </h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
